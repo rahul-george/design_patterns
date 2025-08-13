a3ffa2658f9ed030b360be591c25bc65c68d20bf
Rahul T Gerorge <34202955+rahul-george@users.noreply.github.com>
2025-08-13 09:40:51 +0000

    feat: Added a fallback to another channel logic

--- abstract_method/problem_2/main.py
+++ abstract_method/problem_2/main.py
@@ -20,13 +20,13 @@ class SmsSender(NotificationSender):
     retry_count = 2
 
     def send(self, recipient, message) -> None:
-        print("Email Sent")
+        print("SMS Sent")
             
 
 class EmailSender(NotificationSender):
     retry_count = 3
     def send(self, recipient, message) -> None:
-        print("Failed to send SMS. Gateway time out")
+        print("Failed to send Email. time out")
         raise NotificationSenderError("An error occured")
 
 class PushSender(NotificationSender):
@@ -37,6 +37,8 @@ class PushSender(NotificationSender):
 
 class NotificationManager(ABC):
     def notify(self, recipient, message):
+        """The notify method implements the retry logic,
+          but the retry count itself is stored in the sender classes"""
         sender = self.create_sender()
         _retry = 0
         while _retry < sender.retry_count:
@@ -69,13 +71,49 @@ class PushNotificationManager(NotificationManager):
     def create_sender(self) -> NotificationSender:
         return PushSender()
 
+class FallbackNotifictionManager:
+    def __init__(self, managers: list[NotificationManager]) -> None:
+        self._managers = managers
+
+    def _notify_with_simple_retry(self, mgr, recipient, message):
+        attempt = 0
+        sender: NotificationSender = mgr.create_sender()
+
+        while attempt < sender.retry_count:
+            try:
+                attempt += 1
+                sender.send(recipient, message)
+                return
+            except NotificationSenderError as err:
+                # You can also create two classes one for retriable errors 
+                # and another for permanent errors
+                print(f"Attempt {attempt}/{sender.retry_count}. Retrying...")
+        
+        # If it comes here, it means it did not return
+        raise RetryFailedError("Out of retries")
+    
+    def notify(self, recipient, message):
+        for mgr in self._managers:
+            try:
+                self._notify_with_retry(mgr, recipient, message)
+                return 
+            except RetryFailedError as err:
+                print(err)
+                print("Falling back to next channel")
 
 def main():
-    try:
-        sender = EmailNotificationManager()
-        sender.notify('', '')
-    except RetryFailedError as err:
-        sender = SmsNotificationManager()
-        sender.notify('', '')
+    # try:
+    #     sender = EmailNotificationManager()
+    #     sender.notify('', '')
+    # except RetryFailedError as err:
+    #     sender = SmsNotificationManager()
+    #     sender.notify('', '')
+
+    # With fall back
+    sender_manager_with_fallback = FallbackNotifictionManager([
+        EmailNotificationManager(),
+        SmsNotificationManager()
+    ])
+    sender_manager_with_fallback.notify('', '')
 
 main()
