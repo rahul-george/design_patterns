21ee300049531b92548304e050ced16a1560ae10
Rahul T Gerorge <34202955+rahul-george@users.noreply.github.com>
2025-08-14 18:10:44 +0000

    refactor: Extract the retry logic to a common class. Simple factory in Notification Manager class

--- abstract_method/problem_2/main_2.py
+++ abstract_method/problem_2/main_2.py
@@ -7,7 +7,9 @@ Goal:
 5. SMS notification channel also should have a retry. 
 6. retry is channel specific. 
 """
+from abc import ABC, abstractmethod
 import random
+
 
 class RetryFailedError(Exception):
     pass
@@ -28,50 +30,65 @@ def generate_response(user_choice=None):
         raise PermanentError("Unrecoverable error")
 
 
-class EmailSender():
+class NotificationSender(ABC):
+    max_retry_count = 0
+    @abstractmethod
+    def send(self, recipient, message):
+        pass
+
+
+class EmailSender(NotificationSender):
     max_retry_count = 2
     def __init__(self) -> None:
         pass
 
     def send(self, recipient: str, message: str) -> None:
-        attempt = 0
-        while attempt < self.max_retry_count:
-            try:
-                generate_response(1)
-                print("Email Sent")
-                return
-            except TransientError as err:
-                attempt += 1
-                print("Ran into a temporary error")
-                print(f"Retrying {attempt}/{self.max_retry_count} times.. ")
-        
-        raise RetryFailedError("Exhausted all retry attempts")
+        print("Email Sent")
     
 
-class SmsSender():
+class SmsSender(NotificationSender):
     max_retry_count = 2
     def __init__(self) -> None:
         pass
 
     def send(self, recipient: str, message: str) -> None:
+        print("SMS Sent")
+    
+
+class NotificationManager:
+    def __init__(self) -> None:
+        pass
+
+    def create_sender(self, mode: str) -> NotificationSender:
+        """Delegated the creation of sender object based on the mode parameter to this simple factory"""
+        if mode == 'SMS':
+            return SmsSender()
+        elif mode == 'EMAIL':
+            return EmailSender()
+        else: 
+            raise NotImplementedError(f"{mode} notification channel is not implemented")
+
+    def notify(self, mode: str, recipient: str, message: str):
+        sender = self.create_sender(mode)
+
         attempt = 0
-        while attempt < self.max_retry_count:
+        while attempt < sender.max_retry_count:
             try:
                 generate_response(1)
-                print("SMS Sent")
+                sender.send(recipient, message)
                 return
             except TransientError as err:
                 attempt += 1
                 print("Ran into a temporary error")
-                print(f"Retrying {attempt}/{self.max_retry_count} times.. ")
+                print(f"Retrying {attempt}/{sender.max_retry_count} times.. ")
         
         raise RetryFailedError("Exhausted all retry attempts")
         
 
 def notify(recipient, message):
     """Use the notify function directly in other places"""
-    sender = SmsSender()
-    sender.send(recipient, message)
+    notification_mgr = NotificationManager()
+    notification_mgr.notify('EMAIL', recipient, message)
 
 
 def main():
